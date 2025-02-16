d7c086462caac7a4de088eada4ec6bafa4c58223
Rachel <hurui.lcy@gmail.com>
2025-02-16 14:19:19 -0700

    modules & requires

---
+++ modules.js
@@ -0,0 +1,9 @@
+// const xyz = require('./people');
+const { people,ages } = require('./people');
+
+const os = require('os')
+console.log(os.platform(), os.homedir());
+
+// console.log(people,ages)
+// console.log(people)
+
--- people.js
+++ people.js
@@ -1,1 +1,10 @@
-const people = 
+const people = ['yoshi', 'ryu','chun-li','mario'];
+const ages = [20, 25, 30, 35];
+
+console.log(people)
+
+module.exports = 'Hello';
+module.exports = {
+  people:people,//people: shortcut
+  ages
+};
