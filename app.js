ef30e164f2bc282ce170045f84526af6c583091e
Rachel <hurui.lcy@gmail.com>
2025-02-16 23:53:48 -0700

    view engines-EJS

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ app.listen(3000);
 app.get('/', (req,res) => {
   //res.send('<p> home page </p>')
   // res.sendFile('./views/index.html', {root:__dirname});
-  res.render('index');
+  res.render('index',{title: 'Home'});
 });
 
 app.get('/about', (req,res) => {
   //res.send('<p> about page </p>')
   // res.sendFile('./views/about.html', {root:__dirname});
-  res.render('about');
+  res.render('about',{title: 'About'});
 });
 
 //redirects
@@ -26,11 +26,11 @@ app.get('/about', (req,res) => {
 //   res.redirect('/about')
 // });
 app.get('/blogs/create',(req,res) => {
-  res.render('create')
+  res.render('create',{title: 'Create a new blog'})
 } )
 
 //404 page
 app.use((req,res) => {
   // res.status(404).sendFile('./views/404.html', {root:__dirname})
-  res.status(404).render('404');
+  res.status(404).render('404',{title: '404'});
 })
